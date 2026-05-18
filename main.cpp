287e082daa0d809d320cf4a0a59539324c66aaae
Kacper3215641 <105670438+kapi54312358@users.noreply.github.com>
2026-05-18 20:34:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,46 +2,22 @@
 
 using namespace std;
 
-long long przesun(long long n){
-	long long pom=0, ilo_cyfrowa=0, r=0, a=0, b=0, w=0, i=0, pot=1;
-	pom=n;
-	
-	while(pom>0){
-		ilo_cyfrowa++;
-		pom/=10;	
+int przestaw2(int n){
+	long long w=0, r=0, a=0, b=0, pot=1;
+	while(n>0){
+		b=n%10;
+		n/=10;
+		a=n%10;
+		n/=10;
+		w+=a*pot;
+		pot*=10;
+		w+=b*pot;
+		pot*=10;
 	}
-	
-	if (ilo_cyfrowa%2==1){
-		for(i=0; i<ilo_cyfrowa/2; i++){
-			r=n%100;
-			a=r/10;
-			b=r%10;
-			n/=100;
-			w+=a*pot+b*10*pot;
-			pot*=100;
-		}
-		r=n%100;
-		w+=r*pot;
-	}
-		
-	else{
-		for(i=0; i<ilo_cyfrowa/2; i++){
-			r=n%100;
-			a=r/10;
-			b=r%10;
-			n/=100;
-			w+=a*pot+b*10*pot;
-			pot*=100;
-		}
-	}
-	
-	return w;
+	return w;	
 }
 
 int main(int argc, char** argv) {
-	long long n=0;
-	cout<<"podaj n ";
-	cin>>n;
-	cout<<przesun(n);
+	cout<<przestaw2(316498);
 	return 0;
 }
